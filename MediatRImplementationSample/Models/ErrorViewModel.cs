ec7737aee7662fbbde5e4acb42496de4438c0342
Paulo Pitte <pauloanalista@outlook.com>
2021-08-04 10:15:31 -0300

    Adicionar arquivos de projeto.

---
+++ MediatRImplementationSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MediatRImplementationSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
